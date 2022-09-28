a71ff689f68396e485d970d2cf96b83fb2db02c5
PrincessPride <bukhareal@gmail.com>
2022-09-29 05:23:39 +0600

    edited 1st file

--- index.php
+++ index.php
@@ -4,22 +4,30 @@
 
 // Echo - Output strings, numbers, html, etc
 echo 'Hello';
+echo '<br>';
 echo 123;
+echo '<br>';
 echo '<h1>Hello</h1>';
 
 // print - Similar to echo, but a bit slower
 print 'Hello';
+echo '<br>';
 
 // print_r - Gives a bit more info. Can be used to print arrays
 print_r('Hello');
+echo '<br>';
 print_r([1, 2, 3]);
+echo '<br>';
 
 // var_dump - Even more info like data type and length
 var_dump('Hello');
+echo '<br>';
 var_dump([1, 2, 3]);
+echo '<br>';
 
 // Escaping characters with a backslash
 echo "Is your name O\'reilly?";
+echo '<br>';
 
 /* ------------ Comments ------------ */
 
@@ -45,8 +53,8 @@ echo "Is your name O\'reilly?";
 </head>
 <body>
   <!-- You can output PHP including variables, etc -->
-  <h1>Hello <?php echo 'Brad'; ?></h1>
+  <h1>Hello <?php echo 'Saif'; ?></h1>
   <!-- You may only drop the semi-colon after a statement when the statement is followed immediately by a closing PHP tag ?>. -->
-  <h1>Hello <?= 'Brad' ?></h1>
+  <h1>Hello <?= 'Saif' ?></h1>
 </body>
 </html>
